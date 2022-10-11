6df2da83ec6bb191a6154a830eb2b5c7ce971004
ekatsnelson <elisek222@gmail.com>
2022-10-11 22:45:24 +0000

    Starting changes

--- solution.rb
+++ solution.rb
@@ -14,7 +14,7 @@ puts "Checking the weather at #{user_location}...."
 
 # Get the lat/lng of location from Google Maps API
 
-gmaps_key = ENV.fetch("GMAPS_KEY")
+gmaps_key = ENV.fetch("GMAPS_TOKEN")
 
 gmaps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{user_location}&key=#{gmaps_key}"
 
@@ -41,7 +41,7 @@ puts "Your coordinates are #{latitude}, #{longitude}."
 
 # Get the weather from Dark Sky API
 
-dark_sky_key = ENV.fetch("DARK_SKY_KEY")
+dark_sky_key = ENV.fetch("DARK_SKY_TOKEN")
 
 dark_sky_url = "https://api.darksky.net/forecast/#{dark_sky_key}/#{latitude},#{longitude}"
 
---
+++ umbrella.rb
@@ -0,0 +1,5 @@
+p "Hi! Where are you located?"
+
+user_location = gets.chomp
+
+p user_location
